d7d0bbba0e097a89d3500989419d2e33b711a8c1
chrisJohn404 <chris.s.johnson4@gmail.com>
2014-12-19 08:38:41 -0700

    Version 0.0.18: reverted to old style of exporting driver_const variable, changed modbusMap exports object as well. Doesn't lazy-load either of them. Only lazy-loads the device and driver objects.

--- LabJack-nodejs/lib/labjack_nodejs.js
+++ LabJack-nodejs/lib/labjack_nodejs.js
@@ -9,8 +9,8 @@
 // var driver_const = require('./driver_const');
 var device;
 var driver;
-var driver_const;
-var modbusMap;
+var driver_const = require('./driver_const');
+var modbusMap = require('./json_constants_parser');
 
 
 exports.device = function() {
@@ -21,14 +21,15 @@ exports.driver = function() {
 	driver = require('./driver');
 	return driver.ljmDriver();
 };
-exports.driver_const = function() {
-	driver_const = require('./driver_const');
-	return driver_const;
-};
-exports.modbusMap = function() {
-	modbusMap = require('./json_constants_parser');
-	return modbusMap;
-};
+// exports.driver_const = function() {
+// 	driver_const = 
+// 	return driver_const;
+// };
+// exports.modbusMap = function() {
+// 	modbusMap = 
+// 	return modbusMap;
+// };
 // exports.device = device.labjack;
 // exports.driver = driver.ljmDriver;
-// exports.driver_const = driver_const;
+exports.driver_const = driver_const;
+exports.modbusMap = modbusMap;
