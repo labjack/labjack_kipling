dad28d03a2ce79cfc60f4e06c63a0ba0eb9c913b
MarkGilliland <markg773@gmail.com>
2017-07-24 17:22:12 -0600

    some I2C Examples converted for library

--- lib/switchboard_modules/lua_script_debugger/premade_scripts/I2C/10DOF-adafruit-1604.lua
+++ lib/switchboard_modules/lua_script_debugger/premade_scripts/I2C/10DOF-adafruit-1604.lua
@@ -11,80 +11,11 @@
 --Y gyro = 46014
 --Z gyro = 46016
 
-I2C_Utils= {}
-function I2C_Utils.configure(self, sda, scl, speed, options, slave, debug)--Returns nothing
-  self.sda = sda --FIO0:7 = 0:7, EIO0:7 = 8:15, CIO0:7 = 16:23
-  self.scl = scl --FIO0:7 = 0:7, EIO0:7 = 8:15, CIO0:7 = 16:23
-  self.speed = speed ----0=fastest; 65535=fastest; 65534, 65533, etc. gets slower.
-  self.options = options
-  self.slave = slave --7-bit slave address
-  self.speed = speed
-  self.debugEn = debugEn
-  MB.W(5100, 0, self.sda)
-  MB.W(5101, 0, self.scl)
-  MB.W(5102, 0, self.speed)
-  MB.W(5103, 0, self.options)
-  MB.W(5104, 0, self.slave)
-end
-function I2C_Utils.data_read(self, numBytesRX)--Returns an array of {numAcks, Array of {bytes returned}}
-  local numBytesTX = 0
-  MB.W(5108, 0, numBytesTX)
-  MB.W(5109, 0, numBytesRX)
-  MB.W(5110, 0, 1)
-  local dataRX = MB.RA(5160, 99, numBytesRX)
-  local numAcks = MB.R(5114, 1)
-  return {numAcks, dataRX}
-end
-function I2C_Utils.data_write(self, dataTX)--Returns an array of {NumAcks, errorVal}
-  local numBytesRX = 0
-  local numBytesTX = table.getn(dataTX)
-  MB.W(5108, 0, numBytesTX)
-  MB.W(5109, 0, numBytesRX)
-  local errorVal = MB.WA(5120, 99, numBytesTX, dataTX)
-  MB.W(5110, 0, 1)
-  local numAcks = MB.R(5114, 1)
-  return {numAcks, errorVal}
-end
-function I2C_Utils.data_write_and_read(self, dataTX, numBytesRX)--Returns an array of {numAcks, Array of {bytes returned}, errorVal}
-  local numBytesTX = table.getn(dataTX)
-  MB.W(5108, 0, numBytesTX)
-  MB.W(5109, 0, numBytesRX)
-  local errorVal = MB.WA(5120, 99, numBytesTX, dataTX)
-  MB.W(5110, 0, 1)
-  local numAcks = MB.R(5114, 1)
-  local dataRX = MB.RA(5160, 99, numBytesRX)
-  return {numAcks, dataRX, errorVal}
-end
-function I2C_Utils.calc_options(self, resetAtStart, noStopAtStarting, disableStretching)--Returns a number 0-7
-  self.resetAtStart = resetAtStart
-  self.noStop = noStopAtStarting
-  self.disableStre = disableStretching
-  local optionsVal = 0
-  optionsVal = self.resetAtStart*1+self.noStop*2+self.disableStre*4
-  return optionsVal
-end
-function I2C_Utils.find_all(self, minAddr, maxAddr)--Returns an array of all valid addresses, in number form
-  local validAddresses = {}
-  for slaveAddr = minAddr, maxAddr do
-    MB.W(5104, 0, slaveAddr)
-    local numBytesTX = 0
-    local numBytesRX = 0
-    MB.W(5108, 0, numBytesTX)
-    MB.W(5109, 0, numBytesRX)
-    MB.W(5110, 0, 1)
-    local numAcks = MB.R(5114, 1)
-    if numAcks ~= 0 then
-      table.insert(validAddresses,slaveAddr)
-    end
-    for j = 0, 2000 do end
-  end
-  local addrLen = table.getn(validAddresses)
-  if addrLen == 0 then
-    print("No valid addresses were found")
-  end
-  MB.W(5104, 0, self.slave)
-  return validAddresses
-end
+magAddr = 0x1E
+accelAddr = 0x19
+gyroAddr = 0x6B
+I2C.config(13, 12, 65516, 0, magAddr, 0)--configure the I2C Bus
+
 function convert_16_bit(msb, lsb, conv)--Returns a number, adjusted using the conversion factor. Use 1 if not desired  
   res = 0
   if msb >= 128 then
@@ -95,18 +26,7 @@ function convert_16_bit(msb, lsb, conv)--Returns a number, adjusted using the co
   return res
 end
 
-magI2C = I2C_Utils
-accelI2C = I2C_Utils
-gyroI2C = I2C_Utils
-
-magAddr = 0x1E
-magI2C.configure(magI2C, 13, 12, 65516, 0, magAddr, 0)--configure the I2C Bus
-accelAddr = 0x19
-accelI2C.configure(accelI2C, 13, 12, 65516, 0, accelAddr, 0)--configure the I2C Bus
-gyroAddr = 0x6B
-gyroI2C.configure(accelI2C, 13, 12, 65516, 0, gyroAddr, 0)--configure the I2C Bus
-
-addrs = magI2C.find_all(magI2C, 0, 127)
+addrs = I2C.search(0, 127)
 addrsLen = table.getn(addrs)
 for i=1, addrsLen do--verify that the target device was found     
   if addrs[i] == magAddr then
@@ -122,21 +42,21 @@ end
 
 --init mag slave
 MB.W(5104, 0, magAddr)--change target to magnetometer
-magI2C.data_write(magI2C, {0x00, 0x14})
-magI2C.data_write(magI2C, {0x01, 0x20})
-magI2C.data_write(magI2C, {0x02, 0x00})
+I2C.write({0x00, 0x14})
+I2C.write({0x01, 0x20})
+I2C.write({0x02, 0x00})
 --init accel slave
 MB.W(5104, 0, accelAddr)--change target to accelerometer
-accelI2C.data_write(accelI2C, {0x20, 0x27})
-accelI2C.data_write(accelI2C, {0x23, 0x49})
+I2C.write({0x20, 0x27})
+I2C.write({0x23, 0x49})
 --init gyro slave
 MB.W(5104, 0, gyroAddr)--change target to gyro
-gyroI2C.data_write(gyroI2C, {0x21, 0x00})--1. Write CTRL2
-gyroI2C.data_write(gyroI2C, {0x22, 0x00})--2. Write CTRL3
-gyroI2C.data_write(gyroI2C, {0x23, 0x60})--3. Write CTRL4
-gyroI2C.data_write(gyroI2C, {0x25, 0x00})--5. Write Reference
-gyroI2C.data_write(gyroI2C, {0x24, 0x00})--9. Write CTRL5
-gyroI2C.data_write(gyroI2C, {0x20, 0xBF})--10. Write CTRL1
+I2C.write({0x21, 0x00})--1. Write CTRL2
+I2C.write({0x22, 0x00})--2. Write CTRL3
+I2C.write({0x23, 0x60})--3. Write CTRL4
+I2C.write({0x25, 0x00})--5. Write Reference
+I2C.write({0x24, 0x00})--9. Write CTRL5
+I2C.write({0x20, 0xBF})--10. Write CTRL1
 
 LJ.IntervalConfig(0, 100)
 while true do
@@ -145,9 +65,9 @@ while true do
     MB.W(5104, 0, magAddr)--change target to magnetometer
     dataMagRaw = {}
     for i=0, 5 do
-      magI2C.data_write(magI2C, {0x03+i})
-      dataMagIn = magI2C.data_read(magI2C, 1)[2][1]  
-      table.insert(dataMagRaw, dataMagIn)
+      I2C.write({0x03+i})
+      dataMagIn = I2C.read(1)
+      table.insert(dataMagRaw, dataMagIn[1])
     end
     dataMag = {}
     table.insert(dataMag, convert_16_bit(dataMagRaw[1], dataMagRaw[2], 1100))
@@ -158,9 +78,9 @@ while true do
     MB.W(5104, 0, accelAddr)--change target to accelerometer
     dataAccelRaw = {}
     for i=0, 5 do
-      accelI2C.data_write(accelI2C, {0x28+i})
-      dataAccelIn = accelI2C.data_read(accelI2C, 1)[2][1]  
-      table.insert(dataAccelRaw, dataAccelIn)
+      I2C.write({0x28+i})
+      dataAccelIn = I2C.read(1)
+      table.insert(dataAccelRaw, dataAccelIn[1])
     end
     dataAccel = {}
     table.insert(dataAccel, convert_16_bit(dataAccelRaw[1], dataAccelRaw[2], (0x7FFF/2)))
@@ -171,9 +91,9 @@ while true do
     MB.W(5104, 0, gyroAddr)--change target to accelerometer
     dataGyroRaw = {}
     for i=0, 5 do
-      gyroI2C.data_write(gyroI2C, {0x28+i})
-      dataGyroIn = gyroI2C.data_read(gyroI2C, 1)[2][1]  
-      table.insert(dataGyroRaw, dataGyroIn)
+      I2C.write({0x28+i})
+      dataGyroIn = I2C.read(1)
+      table.insert(dataGyroRaw, dataGyroIn[1])
     end
     dataGyro = {}
     for i=0, 2 do
--- lib/switchboard_modules/lua_script_debugger/premade_scripts/I2C/proximity-VL6180X.lua
+++ lib/switchboard_modules/lua_script_debugger/premade_scripts/I2C/proximity-VL6180X.lua
@@ -3,6 +3,12 @@
 -- Value in mm: 46080
 -- Value in in: 46000
 
+fwver = MB.R(60004, 3)
+if fwver < 1.0224 then
+  print("This lua script requires a firmware version of 1.0224 or higher. Program Stopping")
+  MB.W(6000, 1, 0)
+end
+
 sI = {}
 sI[1] =  {0x02, 0x07, 0x01}
 sI[2] =  {0x02, 0x08, 0x01}
@@ -35,117 +41,10 @@ sI[27] = {0x01, 0xac, 0x3e}
 sI[28] = {0x01, 0xa7, 0x1f}
 sI[29] = {0x00, 0x30, 0x00}
 
-I2C_Utils= {}
-
-function I2C_Utils.configure(self, isda, iscl, ispeed, ioptions, islave, idebug)--Returns nothing   
-  self.sdaNum = isda
-  self.sclNum = iscl
-  self.speed = ispeed
-  self.options = ioptions
-  self.slave = islave
-  self.debugEnable = idebug
-  MB.W(5100, 0, self.sdaNum)
-  MB.W(5101, 0, self.sclNum)
-  MB.W(5102, 0, self.speed)
-  MB.W(5103, 0, self.options)
-  MB.W(5104, 0, self.slave)
-  if self.debugEnable == 1 then
-    print("Configured I2C Bus")
-  end
-end
-function I2C_Utils.data_read(self, inumBytesRX)--Returns an array of {numAcks, Array of {bytes returned}}   
-  self.numBytesRX = inumBytesRX
-  self.numBytesTX = 0
-
-  MB.W(5108, 0, self.numBytesTX)
-  MB.W(5109, 0, self.numBytesRX)
-  
-  MB.W(5110, 0, 1)
-  
-  dataRX = MB.RA(5160, 99, self.numBytesRX)
-  numAcks = MB.R(5114, 1)
-  
-  if self.debugEnable == 1 then
-    dataRXStr = ""
-    for i=1, self.numBytesRX do
-      dataRXStr = dataRXStr.."0x"..string.upper(string.format("%x",dataRX[i])).." "
-    end
-    print("Received "..dataRXStr.."with "..string.format("%d",numAcks).." acks")
-  end
-  
-  return {numAcks, dataRX}--Array of {numAcks, Array of {bytes returned}}
-end
-function I2C_Utils.data_write(self, idataTX)--Returns an array of {NumAcks, errorVal}   
-  self.numBytesRX = 0
-  self.dataTX = idataTX
-  self.numBytesTX = table.getn(self.dataTX)
-  
-  MB.W(5108, 0, self.numBytesTX)
-  MB.W(5109, 0, self.numBytesRX)
-  errorVal = MB.WA(5120, 99, self.numBytesTX, self.dataTX)
-  
-  MB.W(5110, 0, 1)
-  numAcks = MB.R(5114, 1)
-  
-  if self.debugEnable == 1 then
-    dataTXStr = ""
-    for i=1, self.numBytesTX do
-      dataTXStr = dataTXStr.."0x"..string.upper(string.format("%x",self.dataTX[i])).." "
-    end
-    print("Transmitted "..dataTXStr.."with "..string.format("%d",numAcks).." acks")
-  end
-
-  return {numAcks, errorVal}--Array of {NumAcks, errorVal}
-end
-function I2C_Utils.calc_options(self, iresetAtStart, inoStopAtStarting, idisableStretching)--Returns a number 0-7    
-  self.resetAtStart = iresetAtStart
-  self.noStop = inoStopAtStarting
-  self.disableStretching = idisableStretching
-  optionsVal = 0
-  optionsVal = self.resetAtStart*1+self.noStop*2+self.disableStretching*4
-  return optionsVal
-end
-function I2C_Utils.find_all(self, ilower, iupper)--Returns an array of all valid addresses, in number form  
-  validAddresses = {}
-  origSlave = self.slave
-  for i = ilower, iupper do
-    slave = i
-    MB.W(5104, 0, slave)
-    
-    self.numBytesTX = 0
-    self.numBytesRX = 1
-    
-    MB.W(5108, 0, self.numBytesTX)
-    MB.W(5109, 0, self.numBytesRX)
-    MB.W(5110, 0, 1)
-    numAcks = MB.R(5114, 1)
-    
-    if numAcks ~= 0 then
-      table.insert(validAddresses, i)
-      if self.debugEnable == 1 then
-        print("Recieved "..string.format("%d",numAcks).." acks from address ".."0x"..string.format("%x",slave).."(Hex) "..string.format("%d",slave).."(Decimal)")
-      end
-    end
-    for j = 0, 1000 do
-      --delay
-    end
-  end
-  if self.debugEnable == 1 then
-    addrLen = table.getn(validAddresses)
-    if addrLen == 0 then
-      print("No valid addresses were found  over the range "..string.format("%d", ilower).." to "..string.format("%d", iupper))
-    end
-  end
-  MB.W(5104, 0, origSlave)
-  return validAddresses
-end
-
-myI2C = I2C_Utils
-
 SLAVE_ADDRESS = 0x29
-myI2C.configure(myI2C, 13, 12, 65516, tempOptions, SLAVE_ADDRESS, 0)--configure the I2C Bus
+I2C.config(13, 12, 65516, tempOptions, SLAVE_ADDRESS, 0)--configure the I2C Bus
 
-addrs = myI2C.find_all(myI2C, 0, 127)
+addrs = I2C.search(0, 127)
 addrsLen = table.getn(addrs)
 for i=1, addrsLen do--verify that the target device was found     
   if addrs[i] == SLAVE_ADDRESS then
@@ -153,33 +52,39 @@ for i=1, addrsLen do--verify that the target device was found
     break
   end
 end
-
-for i = 1, 29 do--send the neccissarry intialization bytes to the slave    
-  acks = myI2C.data_write(myI2C, sI[i])[1]
-  if acks == 0 then
-    print("Writing sint["..i.."] failed")
-  end
+if addrsLen == 0 then
+  print("No I2C Slave detected, program stopping")
+  MB.W(6000, 1, 0)
 end
-print("Sensor Initialized")
 
-LJ.IntervalConfig(0, 900)             --set interval to 900 for 900ms
+errorOut = 0
+for i = 1, 29 do--send the neccissarry intialization bytes to the slave    
+  errorOut = I2C.write(I2C, sI[i])
+end
+if errorOut == 0 then
+  print("Sensor Initialized")
+else
+  print("Writing sint["..i.."] failed")
+end
+
+LJ.IntervalConfig(0, 500)             --set interval
 stage = 0 --used to monitor program progress
 while true do
   if LJ.CheckInterval(0) then
     if stage == 0 then
-      myI2C.data_write(myI2C, {0x00, 0x18, 0x03})
-      LJ.IntervalConfig(0, 40)       --set interval to 100 for 100ms to give the range finder some processing time
+      I2C.write({0x00, 0x18, 0x03})
+      LJ.IntervalConfig(0, 40)       --set interval to give the range finder some processing time
       stage = 1
     elseif stage == 1 then
-      myI2C.data_write(myI2C, {0x00, 0x62})
-      LJ.IntervalConfig(0, 40)       --set interval to 100 for 100ms to give the range finder some processing time
+      I2C.write({0x00, 0x62})
+      LJ.IntervalConfig(0, 40)       --set interval to give the range finder some processing time
       stage = 2
     elseif stage == 2 then
-      distRaw = myI2C.data_read(myI2C, 2)[2]
+      distRaw = I2C.read(2)
       MB.W(46080, 2, distRaw[2])--Store value, in mm, for user to access with another program, such as LabVIEW or Python
-      MB.W(46000, 3,  distRaw[2]/25.4)--Store value in in.
+      MB.W(46000, 3,  distRaw[2]/25.4)--Store value in inches
       print("Distance: "..string.format("%.2f", distRaw[2]).."mm "..string.format("%.2f", distRaw[2]/25.4).."in")
-      LJ.IntervalConfig(0, 420)       --reset interval to 900ms
+      LJ.IntervalConfig(0, 420)       --reset interval
       stage = 0
     end
   end
