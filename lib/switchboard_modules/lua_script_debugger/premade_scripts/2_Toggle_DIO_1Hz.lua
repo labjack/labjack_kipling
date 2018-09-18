02c3d5cbb2f32c011da5ce3f8f0098f941c2fc96
chrisJohn404 <chris.s.johnson4@gmail.com>
2018-09-18 00:17:48 -0600

    Add writes to USER_RAM0_F32 & 1.

--- lib/switchboard_modules/lua_script_debugger/premade_scripts/2_Toggle_DIO_1Hz.lua
+++ lib/switchboard_modules/lua_script_debugger/premade_scripts/2_Toggle_DIO_1Hz.lua
@@ -18,10 +18,14 @@ while true do
       high = 0
       mbWrite(outPin, 0, 0)        --write 0 to FIO3 (FIO5 on T4). Address is 2003, type is 0, value is 0(output low)
       print(high, "low")
+      mbWrite(46000, 3, 0)        -- Set register "USER_RAM0_F32".  Address 46000, type 3
+      mbWrite(46002, 3, 200)      -- Set register "USER_RAM1_F32".  Address 46002, type 3
     else
       high = 1
       mbWrite(outPin, 0, 1)        --write 1 to FIO3 (FIO5 on T4). Address is 2003, type is 0, value is 1(output high)
       print(high, "high")
+      mbWrite(46000, 3, 200)      -- Set register "USER_RAM0_F32".  Address 46000, type 3
+      mbWrite(46002, 3, 0)        -- Set register "USER_RAM1_F32".  Address 46002, type 3
     end
   end
 end
