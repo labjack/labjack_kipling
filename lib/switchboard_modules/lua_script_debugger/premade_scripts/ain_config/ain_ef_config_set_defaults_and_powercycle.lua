00d58945499d82d55cd2de4c8cde4a09dc4b1560
sj <soloman.jarman@gmail.com>
2019-10-15 13:11:33 -0600

    Fixed up formatting of the AIN EF config and powercycle script

--- lib/switchboard_modules/lua_script_debugger/premade_scripts/ain_config/ain_ef_config_set_defaults_and_powercycle.lua
+++ lib/switchboard_modules/lua_script_debugger/premade_scripts/ain_config/ain_ef_config_set_defaults_and_powercycle.lua
@@ -1,47 +1,76 @@
--- This is a general AIN_EF config example that shows the basic process for
--- configuring any AIN_EF feature on a T-Series device, saving them as defaults
--- and then powercycling the device.  This example configures AIN0 and AIN2 to
--- enable the Average/Min/Max extended feature.
+--[[
+    Name: ain_ef_config_set_defaults_and_powercycle.lua
+    Desc: This is a general AIN_EF config example that shows the basic process
+          for configuring any AIN_EF feature on a T-Series device, saving them
+          as defaults, and powercycling the device. This example configures
+          AIN0 and AIN2 to enable the Average/Min/Max extended feature.
+          For a list of all AIN_EF options, see the T-Series datasheet:
+            https://labjack.com/support/datasheets/t-series/ain/extended-features
 
--- For a list of all AIN_EF options, see the T-Series datasheet:
--- https://labjack.com/support/datasheets/t-series/ain/extended-features
-print("Generic Config AIN_EF & set power-up defaults")
+    Note: This should not be run multiple times. It will gradually cause flash wear
+--]]
 
--- Note: THIS SHOULD NOT BE RUN MULTIPLE TIMES. IT WILL SLOWLY CAUSE FLASH WEAR.
+-- Assign functions locally for faster processing
+local modbus_write = MB.W
 
--- Generic function that can be used to configure general analog input settings 
--- such as Range, Resolution, and Settling.  More information about these 
--- settings can be found on the LabJack website under the AIN section:
--- https://labjack.com/support/datasheets/t-series/ain
-function ainEFConfig(chNum, index, cfgA, cfgB, cfgC, cfgD, cfgE, cfgF, cfgG, cfgH, cfgI, cfgJ)
-	MB.W(9000 + chNum * 2, 1, 0) -- Disable AIN_EF
-	MB.W(9000 + chNum * 2, 1, index) -- Enable AIN_EF
-	MB.W(9300 + chNum * 2, 1, cfgA)
-	MB.W(9600 + chNum * 2, 1, cfgB)
-	MB.W(9900 + chNum * 2, 1, cfgC)
-	MB.W(10200 + chNum * 2, 3, cfgD)
-	MB.W(10500 + chNum * 2, 3, cfgE)
-	MB.W(10800 + chNum * 2, 3, cfgF)
-	MB.W(11100 + chNum * 2, 3, cfgG)
-	MB.W(11400 + chNum * 2, 3, cfgH)
-	MB.W(11700 + chNum * 2, 3, cfgI)
-	MB.W(12000 + chNum * 2, 3, cfgJ)
+-------------------------------------------------------------------------------
+--  Desc: Generic function that can be used to configure general analog input
+--        settings such as range, resolution, and settling. More information
+--        about these settings can be found on the LabJack website under the
+--        AIN section:
+--          https://labjack.com/support/datasheets/t-series/ain
+-------------------------------------------------------------------------------
+local function ain_ef_config(
+  channelnum,
+  index,
+  configa,
+  configb,
+  configc,
+  configd,
+  confige,
+  configf,
+  configg,
+  configh,
+  configi,
+  configj
+)
+  -- Disable AIN_EF
+  modbus_write(9000 + channelnum * 2, 1, 0)
+  -- Enable AIN_EF
+  modbus_write(9000 + channelnum * 2, 1, index)
+  modbus_write(9300 + channelnum * 2, 1, configa)
+  modbus_write(9600 + channelnum * 2, 1, configb)
+  modbus_write(9900 + channelnum * 2, 1, configc)
+  modbus_write(10200 + channelnum * 2, 3, configd)
+  modbus_write(10500 + channelnum * 2, 3, confige)
+  modbus_write(10800 + channelnum * 2, 3, configf)
+  modbus_write(11100 + channelnum * 2, 3, configg)
+  modbus_write(11400 + channelnum * 2, 3, configh)
+  modbus_write(11700 + channelnum * 2, 3, configi)
+  modbus_write(12000 + channelnum * 2, 3, configj)
 end
 
-ainChannels = {0, 2} -- Enable AIN0 and AIN2
-numSamples = 200
-scanRate = 6000
+print("Generic Config AIN_EF & set power-up defaults")
+-- Enable AIN0 and AIN2
+local ainchannels = {0, 2}
+-- Index 3 is the max,min, average feature
+local index = 3
+local numsamples = 200
+-- Set the device to scan at 6000 samples per second
+local scanrate = 6000
 
 -- Configure each analog input
-for i=1,table.getn(ainChannels) do
-    ainEFConfig(ainChannels[i], 3,numSamples,0,0,scanRate,0,0,0,0,0,0)
+for i=1,table.getn(ainchannels) do
+    ain_ef_config(ainchannels[i], index,numsamples,0,0,scanrate,0,0,0,0,0,0)
 end
 
 -- Set as power-up default
 print("Saving settings as power-up defaults")
-MB.W(49002, 1, 1)
+modbus_write(49002, 1, 1)
 
 -- Re-set device
 print("Rebooting Device")
-MB.W(61998, 1, 0x4C4A0004)
+-- Write to SYSTEM_REBOOT so the system reboots after 200ms (last 4 bytes of
+-- the write value tells the device how many 50ms ticks to wait before reboot)
+modbus_write(61998, 1, 0x4C4A0004)
 
