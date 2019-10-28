941422b3653c26bc0d2334a540882939c0d9060b
sj <soloman.jarman@gmail.com>
2019-10-28 11:49:16 -0600

    Fixed up the DIO toggle example

--- lib/switchboard_modules/lua_script_debugger/premade_scripts/1_get_device_temperature.lua
+++ lib/switchboard_modules/lua_script_debugger/premade_scripts/1_get_device_temperature.lua
@@ -1,22 +1,30 @@
-print("Read and display the device temperature at 0.5 Hz.")
-local tempK = 0
-local tempF = 0
+--[[
+    Name: 1_get_device_temperature.lua
+    Desc: This example shows how to read the internal device temperature
+    Note: This example requires firmware 1.0282 (T7) or 1.0023 (T4)
+--]]
 
-MB.W(48005, 0, 1)                   --Ensure analog is on
-
-LJ.IntervalConfig(0, 500)           --Configure interval
-local checkInterval=LJ.CheckInterval
-
-function ConvertToF(degK)
-  local degF = 0
-  degF = (degK - 273.15) * 1.8000 + 32.00
-  return degF
+------------------------------------------
+-- Desc: Converts Kelvin to Fahrenheit
+------------------------------------------
+local function convert_k_to_f(degk)
+  local degf = (degk - 273.15) * 1.8000 + 32.00
+  return degf
 end
 
+print("Read and display the device temperature at 0.5 Hz.")
+-- Write 1 to the POWER_AIN register to ensure the analog input module is on
+MB.writeName("POWER_AIN", 1)
+
+-- Configure an interval of 500ms
+LJ.IntervalConfig(0, 500)
+-- Run the program in an infinite loop
 while true do
-  if checkInterval(0) then     --interval finished
-    tempK = MB.R(60052, 3)       --read address 60052 TEMPERATURE_DEVICE_K, type is 3
-    tempF = ConvertToF(tempK)
-    print(tempF, "°F")
+    -- If an interval is done
+  if LJ.CheckInterval(0) then
+    -- Get the temperature in K from the TEMPERATURE_DEVICE_K register
+    local tempk = MB.readName("TEMPERATURE_DEVICE_K")
+    local tempf = convert_k_to_f(tempk)
+    print(tempf, "°F")
   end
 end
