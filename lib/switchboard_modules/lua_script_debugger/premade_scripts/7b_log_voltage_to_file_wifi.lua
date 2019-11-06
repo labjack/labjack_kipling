5e0314a578fb81e79361b739d477f835738b6bc9
sj <soloman.jarman@gmail.com>
2019-11-06 15:54:26 -0700

    Fixed up the formatting of the voltage logging over WiFi example

--- lib/switchboard_modules/lua_script_debugger/premade_scripts/7b_log_voltage_to_file_wifi.lua
+++ lib/switchboard_modules/lua_script_debugger/premade_scripts/7b_log_voltage_to_file_wifi.lua
@@ -1,13 +1,24 @@
-print("Log voltage to file.  Voltage measured on AIN1 every 50ms.  Store values every 5 seconds")
---Requires micro SD Card installed inside the T7 or T7-Pro.
---Requires FW 1.0150 or newer.
---This example is for logging to file while using WiFi, since WiFi needs 5s or more to initialize
---without comm. to/from the uSD card. http://labjack.com/support/datasheets/t7/sd-card
---Timestamp (real-time-clock) available on T7-Pro only
---Some helpful Lua file operations in section 5.7 http://www.lua.org/manual/5.1/manual.html#5.7
---Some file info docs in 21.2 of the Lua documentation http://www.lua.org/pil/21.2.html
+--[[
+    Name: 7b_log_voltage_to_file_wifi.lua
+    Desc: This example shows how to log voltage measurements to file if
+          communicating over WiFi (WiFi needs 5s or more to initialize)
+without comm
+    Note: Requires an SD Card installed inside the T7 or T7-Pro
 
-local hardware = MB.R(60010, 1)
+          This example requires firmware 1.0282 (T7)
+
+          Timestamp (real-time-clock) available on T7-Pro only
+
+          Some helpful Lua file operations in section 5.7:
+            http://www.lua.org/manual/5.1/manual.html#5.7
+
+          Some file info docs in 21.2 of the Lua documentation:
+            http://www.lua.org/pil/21.2.html
+--]]
+
+print("Log voltage to file.  Voltage measured on AIN1 every 50ms.  Store values every 5 seconds")
+-- Get info on the hardware that is installed
+local hardware = MB.readName("HARDWARE_INSTALLED")
 local passed = 1
 if(bit.band(hardware, 8) ~= 8) then
   print("uSD card not detected")
@@ -23,88 +34,103 @@ if(bit.band(hardware, 2) ~= 2) then
 end
 if(passed == 0) then
   print("This Lua script requires an RTC module, Wifi, and a microSD card, but one or many are not detected. These features are only preinstalled on the T7-Pro. Script Stopping")
-  MB.W(6000, 1, 0)--stop script
+  -- Writing a 0 to LUA_RUN stops the script
+  MB.writeName("LUA_RUN", 0)
 end
 
-local Filepre = "/FWi_"
-local Filesuf = ".csv"
-local NumFn = 0
-local Filename = Filepre..string.format("%02d", NumFn)..Filesuf
+local filepre = "/FWi_"
+local filesuf = ".csv"
+local numfn = 0
+local filename = filepre..string.format("%02d", numfn)..filesuf
 local voltage = 0
-local indexVal = 1
+local indexval = 1
 local delimiter = ","
-local dateStr = ""
-local voltageStr = ""
+local strdate = ""
+local strvoltage = ""
 local f = nil
 
-local mbRead=MB.R			--local functions for faster processing
-local mbReadArray=MB.RA
+-- Use a 50ms interval for writing DAC values
+local dacinterval = 50
+-- Use a 5s interval for the sd card
+local sdinterval = 5000
+local numdacs = math.floor(sdinterval/dacinterval)
 
-local DACinterval = 50     --interval in ms, 50 for 50ms, should divide evenly into SDCard interval
-local SDCardinterval = 5000 --inerval in ms, 5000 for 5 seconds
-local numDAC = math.floor(SDCardinterval/DACinterval)
+local data = {}
+local strings = {}
 
-local DataTable = {}
-local stringTable = {}
-
-for i=1, numDAC do
-  DataTable[i] = 0
-  stringTable[i] = "bar"
+for i=1, numdacs do
+  data[i] = 0
+  strings[i] = "bar"
 end
 
 
-local dateTbl = {}
-dateTbl[1] = 0    --year
-dateTbl[2] = 0    --month
-dateTbl[3] = 0    --day
-dateTbl[4] = 0    --hour
-dateTbl[5] = 0    --minute
-dateTbl[6] = 0    --second
+local date = {}
+date[1] = 0    --year
+date[2] = 0    --month
+date[3] = 0    --day
+date[4] = 0    --hour
+date[5] = 0    --minute
+date[6] = 0    --second
 
+-- Make sure analog is on
+MB.writeName("POWER_AIN",1)
 
-MB.W(48005,0,1)                         --ensure analog is on
+LJ.IntervalConfig(0, dacinterval)
+LJ.IntervalConfig(1, sdinterval)
 
-LJ.IntervalConfig(0, DACinterval)       
-LJ.IntervalConfig(1, SDCardinterval)
-local checkInterval=LJ.CheckInterval
-
-f = io.open(Filename, "r")
+f = io.open(filename, "r")
 if f ~= nil then
   f:close()
-  f = io.open(Filename, "a+")           --File exists, Append to file
+  -- If the file exists, Append to file
+  f = io.open(filename, "a+")
   print ("Appending to file")
 else
-  f = io.open(Filename, "w")            --Create or replace file
+  -- Create or overwrite the file
+  f = io.open(filename, "w")
   print ("Creating new file")
 end
 
-
 while true do
-  if checkInterval(0) then           --DAC interval completed
-    DataTable[indexVal] = mbRead(2, 3)    --voltage on AIN1, address is 2, type is 3
-    dateTbl, error = mbReadArray(61510, 0, 6) --Read the RTC timestamp, -Pro only
-    print("AIN1: ", DataTable[indexVal], "V")
-    dateStr = string.format("%04d/%02d/%02d %02d:%02d.%02d", dateTbl[1], dateTbl[2], dateTbl[3], dateTbl[4], dateTbl[5], dateTbl[6])
-    voltageStr = string.format("%.6f", DataTable[indexVal])
-    stringTable[indexVal] = dateStr..delimiter..voltageStr.."\n"
-    indexVal = indexVal + 1
+  -- If a DAC interval is done
+  if LJ.CheckInterval(0) then
+    data[indexval] = MB.readName("AIN1")
+    -- Read the RTC timestamp, (T7-Pro only)
+    date, error = MB.RA(61510, 0, 6)
+    print("AIN1: ", data[indexval], "V")
+    strdate = string.format(
+      "%04d/%02d/%02d %02d:%02d.%02d",
+      date[1],
+      date[2],
+      date[3],
+      date[4],
+      date[5],
+      date[6]
+    )
+    strvoltage = string.format("%.6f", data[indexval])
+    strings[indexval] = strdate..delimiter..strvoltage.."\n"
+    indexval = indexval + 1
   end
-  if checkInterval(1) then         --file write interval completed -> 5s for WiFi safe
+  -- If the interval wait before writing to an sd card is done
+  if LJ.CheckInterval(1) then
     local i = 1
     local fg = 0
-    indexVal = 1
-    fg = LJ.CheckFileFlag()           --host software wants to read LUAs active file? Is FILE_IO_LUA_SWITCH_FILE=1?
+    indexval = 1
+    -- Check if the host software wants to read the active, opened file
+    fg = LJ.CheckFileFlag()
+    -- If so, increment filename and start writing to a new file
     if fg == 1 then
-      NumFn = NumFn + 1               --increment filename
-      Filename = Filepre..string.format("%02d", NumFn)..Filesuf
+      numfn = numfn + 1
+      filename = filepre..string.format("%02d", numfn)..filesuf
       f:close()
-      LJ.ClearFileFlag()              --inform host that previous file is available. Sets FILE_IO_LUA_SWITCH_FILE=0
-      f = io.open(Filename, "w")      --create or replace new file
+      -- Inform the host computer that previous file is available
+      LJ.ClearFileFlag()
+      --create or replace a new file
+      f = io.open(filename, "w")
       print ("Command issued by host to create new file")
     end
     print ("Appending to file")
-    for i=1, numDAC do
-      f:write(stringTable[i])
+    for i=1, numdacs do
+      f:write(strings[i])
     end
   end
 end
