1180fb48987504a93bbde96caebab7707daceea1
rolsen <rory@gmail.com>
2020-07-15 16:28:05 -0600

    No longer catching and ignoring the process uncaughtException event.
    It was causing it to be harder to view the stack trace when a startup error occurred.
    Also, it's not meant to be caught and ignored: https://nodejs.org/docs/latest-v8.x/api/process.html#process_warning_using_uncaughtexception_correctly

--- ljswitchboard-splash_screen/lib/main.js
+++ ljswitchboard-splash_screen/lib/main.js
@@ -7,18 +7,8 @@ var startupAttributes = {
 	'execPath': process.execPath,
 };
 exports.info = startupAttributes;
-var catchUncaughtExceptions = function(err) {
-	console.error('in catchUncaughtExceptions', err);
-};
+
 var catchWindowErrors = function(err) {
 	console.error('in catchWindowErrors', err);
 };
-
-// process.on('exit', function() {
-// 	console.log('Goodbye!');
-// });
-// process.on('SIGINT', function() {
-// 	console.log('Got SIGINT.  Press Control-D to exit');
-// });
-process.on('uncaughtException', catchUncaughtExceptions);
 window.addEventListener('error' ,catchWindowErrors);
