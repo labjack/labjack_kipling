5cc3ff8eefe01452742e08dc6e7d4eb09590c529
rolsen <rory@labjack.com>
2020-08-13 09:54:40 -0600

    ljswitchboard-builder: edit_package_keys cleanup and test.

--- ljswitchboard-builder/test/test.js
+++ ljswitchboard-builder/test/test.js
@@ -1,7 +1,12 @@
 
 var ZIPPING_TEST = true;
+var EDIT_PACKAGE_KEYS_TEST = true;
 
 if(ZIPPING_TEST) {
 	var zipping_test = require('./zipping_test');
 	exports.zipping_test = zipping_test.tests;
+}
+if(EDIT_PACKAGE_KEYS_TEST) {
+	var edit_package_keys_test = require('./edit_package_keys_test');
+	exports.edit_package_keys_test = edit_package_keys_test.tests;
 }
