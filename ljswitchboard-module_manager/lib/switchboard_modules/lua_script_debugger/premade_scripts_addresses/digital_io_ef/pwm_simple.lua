2726b0121e01327df67a36e628f88bdee697a2b4
Soloman J <soloman.jarman@gmail.com>
2020-10-22 12:52:32 -0600

    Added addresses version of new PWM lua script

--- ljswitchboard-module_manager/lib/switchboard_modules/lua_script_debugger/premade_scripts_addresses/digital_io_ef/pwm_simple.lua
+++ ljswitchboard-module_manager/lib/switchboard_modules/lua_script_debugger/premade_scripts_addresses/digital_io_ef/pwm_simple.lua
@@ -1,8 +1,7 @@
 --[[
     Name: pwm_simple.lua
     Desc: This example shows how to set up a pwm output on FIO0
-    Note: This example requires firmware 1.0282 (T7) or 1.0023 (T4)
-    	  	This script avoids truncation errors in lua by writing 32-bit registers as two 
+    Note: This script avoids truncation errors in lua by writing 32-bit registers as two 
   		  	16-bit values. Some 32-bit registers such as DIO#_EF_ENABLE could never have
   		  	truncation errors; we still write them as two 16-bit values for consistency
   		  	See the discussion on writing 32-bit registers here:
@@ -34,24 +33,24 @@ local configa = rollval*dutycycle/100
 
 print(string.format("Configuring PWM with %dHz frequency and %d%% duty cycle...\n", freq, dutycycle))
 -- Disable the clock 0 source before configuring
-MB.writeName("DIO_EF_CLOCK0_ENABLE", 0)
+MB.W(44900,U16_TYPE_CONSTANT, 0)
 -- Use a clock divisor of 1
-MB.writeName("DIO_EF_CLOCK0_DIVISOR", divisor)
+MB.W(44901, U16_TYPE_CONSTANT, divisor)
 -- Set the roll value
-MB.writeNameArray("DIO_EF_CLOCK0_ROLL_VALUE", NUM_U16_PER_U32, u32_to_u16_array(rollval), U16_TYPE_CONSTANT)
+MB.WA(44904, U16_TYPE_CONSTANT, NUM_U16_PER_U32, u32_to_u16_array(rollval))
 -- Re-enable the clock 0 source
-MB.writeName("DIO_EF_CLOCK0_ENABLE", 1)
+MB.W(44900, U16_TYPE_CONSTANT, 1)
 
 -- Disable the DIO_EF before configuring
-MB.writeNameArray("DIO0_EF_ENABLE", NUM_U16_PER_U32, u32_to_u16_array(0), U16_TYPE_CONSTANT)
+MB.WA(44000, U16_TYPE_CONSTANT, NUM_U16_PER_U32, u32_to_u16_array(0))
 -- Set index to 0 for PWM
-MB.writeNameArray("DIO0_EF_INDEX", NUM_U16_PER_U32, u32_to_u16_array(0), U16_TYPE_CONSTANT)
+MB.WA(44100, U16_TYPE_CONSTANT, NUM_U16_PER_U32, u32_to_u16_array(0))
 -- Set options to 0 for using clock source 0
-MB.writeNameArray("DIO0_EF_OPTIONS", NUM_U16_PER_U32, u32_to_u16_array(0), U16_TYPE_CONSTANT)
+MB.WA(44200, U16_TYPE_CONSTANT, NUM_U16_PER_U32, u32_to_u16_array(0))
 -- Set the high to low transition point
-MB.writeNameArray("DIO0_EF_CONFIG_A", NUM_U16_PER_U32, u32_to_u16_array(configa), U16_TYPE_CONSTANT)
+MB.WA(44300, U16_TYPE_CONSTANT, NUM_U16_PER_U32, u32_to_u16_array(configa))
 -- Enable the feature to start outputting the PWM
-MB.writeNameArray("DIO0_EF_ENABLE", NUM_U16_PER_U32, u32_to_u16_array(1), U16_TYPE_CONSTANT)
+MB.WA(44000, U16_TYPE_CONSTANT, NUM_U16_PER_U32, u32_to_u16_array(1))
 
 -- Configure an interval of 2000ms
 -- After 2000ms change the PWM frequency
@@ -72,11 +71,11 @@ while running do
 		print("Halved the PWM frequency...")
 		-- Halve the PWM frequency
 		rollval = rollval*2
-		MB.writeNameArray("DIO_EF_CLOCK0_ROLL_VALUE", NUM_U16_PER_U32, u32_to_u16_array(rollval), U16_TYPE_CONSTANT)
+		MB.WA(44904, U16_TYPE_CONSTANT, NUM_U16_PER_U32, u32_to_u16_array(rollval))
 		-- update the configa value according to the new rollval
 		-- Below will maintain the same initial duty cycle
 		configa = rollval*dutycycle/100
-		MB.writeNameArray("DIO0_EF_CONFIG_A", NUM_U16_PER_U32, u32_to_u16_array(configa), U16_TYPE_CONSTANT)
+		MB.WA(44300, U16_TYPE_CONSTANT, NUM_U16_PER_U32, u32_to_u16_array(configa))
 		numupdates = numupdates + 1
 	end
 end
@@ -84,4 +83,4 @@ end
 
 print("\nScript finished")
 -- Stop the script
-MB.writeNameArray("LUA_RUN", NUM_U16_PER_U32, u32_to_u16_array(0), U16_TYPE_CONSTANT)
+MB.W(6000, 1, 0)
