5d7deca66d6879c5ce94622dd78489671a44b43c
Jimmy Brant <jdougco@gmail.com>
2022-09-12 14:01:49 -0600

    More UI stuff

--- ljswitchboard-module_manager/lib/switchboard_modules/simple_logger/controller.js
+++ ljswitchboard-module_manager/lib/switchboard_modules/simple_logger/controller.js
@@ -8,24 +8,41 @@
  * Goals for the Simple Logger module:
 **/
 
-const { time } = require('console');
-const fs = require('fs')
+const { time }           = require('console');
+const fs                 = require('fs')
+const device_manager     = require('ljswitchboard-device_manager')
+var CREATE_SIMPLE_LOGGER = require("ljswitchboard-simple_logger");
+const { dirname }        = require('path');
 
-const device_manager = require('ljswitchboard-device_manager')
+const package_loader = global.package_loader;
+const fs_facade = package_loader.getPackage('fs_facade');
 
-
+var REGISTERS_DATA_SRC = 'simple_logger/ljm_constants.json';
 
 // Constant that determines device polling rate.  Use an increased rate to aid
 // in user experience.
 var MODULE_UPDATE_PERIOD_MS = 1000; 
 
-var logger = require("ljswitchboard-simple_logger");
-const { dirname } = require('path');
+/**
+ * Handles the user signaling to start the simple logger
+ * from the start button press, this is the @onClick method
+ */
+function userStartLogger() {
+
+}
+
+
+
+
+
+
 
 function dispFile(contents) {
-    $("#configView").click()
+    // $("#configViewButton").click()
     // $("#configForm").load("./templates/view_current_configuration.html");
-    document.getElementById('currentConfig').textContent=JSON.stringify(contents, undefined, 2)
+    document.getElementById("currentConfigDisp").style.visibility = "visible";
+    document.getElementById('currentConfigDisp').textContent=JSON.stringify(contents, undefined, 2)
+
 }
 
 /**  opens the file explorer to look for .json config files 
@@ -75,7 +92,7 @@ function openFile(func) {
 *   it displays an error window
 */
 function verifyConfigFile(filepath) {
-    logger.verifyConfigFile(filepath)
+    CREATE_SIMPLE_LOGGER.verifyConfigFile(filepath)
     .catch(err => alert("Invalid Logger Configuration File", err))
 
 }
@@ -114,6 +131,25 @@ function module() {
         'active': 'dashboard_mode',
         'configure': 'configure_mode',
     };
+
+    /**
+     * Get information about the registers that can be logged / watched.
+     *
+     * Load register information for all devices, a complete record of registers
+     * that can be logged or watched.
+     *
+     * @return {Promise} A promise that resolves to an Object with register
+     *      information. See the registers section of the ljm_constants JSON file.
+    **/
+    function getRegisters()
+    {
+        return new Promise((resolve) => {
+            var registerInfoSrc = fs_facade.getExternalURI(REGISTERS_DATA_SRC);
+            fs_facade.getJSON(registerInfoSrc, genericErrorHandler, function (info) {
+                resolve(info['registers']);
+            });
+        });
+    }
     
     /**
      * Function is called once every time the module tab is selected, loads the module.
@@ -192,3 +228,152 @@ function module() {
 // },deviceManager.getDevices());
 
 // console.log("Done Creating Config")
+function loggerApp() {
+	this.simpleLogger;
+	this.deviceManager;
+	this.logConfigs;
+
+	this.initializeLogger = function(){
+		debugLog('--- In Func: initializeLogger');
+		var defered = q.defer();
+
+		self.simpleLogger = simple_logger.create();
+		attachListeners(self.simpleLogger);
+
+		// Intentionally delay to allow user to read start-up message.
+		setTimeout(function() {
+			self.simpleLogger.initialize()
+			.then(function(res) {
+				debugLog('--- App Initialized',res);
+				defered.resolve();
+			}, function(err) {
+				console.error('Failed to initialize the logger',err);
+				defered.resolve();
+			});
+		}, 5000);
+		
+		
+		return defered.promise;
+	};
+	this.initializeDeviceManager = function() {
+		debugLog('--- In Func: initializeDeviceManager');
+		var defered = q.defer();
+		self.deviceManager = device_manager.create();
+
+		self.deviceManager.connectToDevices([{
+			dt:'LJM_dtANY',
+			ct:'LJM_ctANY',
+			id:'LJM_idANY',
+		}])
+		.then(function(res) {
+			debugLog('--- Connected to devices!',res);
+			defered.resolve();
+		}, function(err) {
+			console.error('Failed to connect to devices',err);
+			defered.resolve();
+		});
+		return defered.promise;
+	};
+	this.updateDeviceListing = function() {
+		debugLog('--- In Func: updateDeviceListing');
+		debugLog('Connected Devices', self.deviceManager.getDevices());
+		var defered = q.defer();
+		self.simpleLogger.updateDeviceListing(self.deviceManager.getDevices())
+		.then(function(res) {
+			debugLog('Device listing has been passwd to the logger',res);
+			defered.resolve();
+		}, function(err) {
+			console.error('Failed to save the device listing to the logger',err);
+			defered.resolve();
+		});
+		return defered.promise;
+	};
+	this.configureLogger = function() {
+		debugLog('--- In Func: configureLogger');
+		var defered = q.defer();
+
+		/*
+		Old config:
+		{
+			'configType': 'filePath',
+			'filePath': logger_config_file_path
+		}
+		*/
+		self.logConfigs = simple_logger.generateBasicConfig({
+			'same_vals_all_devices': true,
+			'registers': ['AIN0','AIN1'],
+			'update_rate_ms': 100,
+		},self.deviceManager.getDevices());
+
+		var fs = require('fs');
+		fs.writeFile(template_logger_config_file,JSON.stringify(self.logConfigs,null,2),function(err) {
+			if(err) {
+				console.error('Error saving generated configs');
+			} else {
+				debugLog('Data was appended to the config file.');
+			}
+			self.simpleLogger.configureLogger({
+				'configType': 'filePath',
+				'filePath': template_logger_config_file
+			})
+			// self.simpleLogger.configureLogger({
+			// 	configType: 'object',
+			// 	configData: self.logConfigs,
+			// 	filePath: cwd
+			// })
+			.then(function(res) {
+				debugLog('Logger has been configured.',res)
+				defered.resolve();
+			}, function(err) {
+				console.error('Logger failed to be configured.',err)
+				defered.resolve();
+			});
+		});
+
+		
+
+
+		
+		return defered.promise;
+	}
+	this.startLogger = function() {
+		debugLog('--- In Func: startLogger');
+		var defered = q.defer();
+		self.simpleLogger.once(eventMap.STOPPED_LOGGER, function(stopData) {
+			debugLog('Logger Stopped');
+			// console.log('(hello_world.js) Logger has stopped!!')
+			defered.resolve();
+		});
+		self.simpleLogger.startLogger()
+		.then(function succ() {
+			
+			debugLog('Logger Started');
+		}, function err() {
+			
+			debugLog('Logger Started');
+		});
+		return defered.promise;
+	}
+	this.waitForLoggerToRun = function() {
+		debugLog('--- In Func: waitForLoggerToRun');
+		var defered = q.defer();
+		defered.resolve();
+		return defered.promise;
+	}
+	this.closeDevices = function() {
+		var defered = q.defer();
+		self.deviceManager.closeDevices()
+		.then(defered.resolve, defered.reject);
+		return defered.promise;
+	}
+	this.finish = function() {
+		debugLog('--- In Func: finish');
+		var defered = q.defer();
+		// setTimeout(defered.resolve, 1000);
+		defered.resolve();
+		return defered.promise;
+	}
+	var self = this;
+}
+
+var loggerApp = new loggerApp()
